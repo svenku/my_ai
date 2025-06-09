838ff31a3e399e1f086b140f7def5936f7ad9c92
svenku <sven.kunsing@gmail.com>
2025-06-10 01:02:26 +0300

    Small adjustments

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-def generate_text(contents=None, model= "gemini-2.0-flash-001"):
-    """Generate text using the Gemini API."""
-    response = client.models.generate_content(
-        model=model,
-        contents=contents
-    )
-    return response
 
 def main():
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    print("Generating text...")
-    response = generate_text(prompt)
-    print("Generated Text:")
-    print(response.text)
-    print("Response Usage Metadata:")
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    try:    
+        prompt = sys.argv[1]
+        print("Generating text...")
+        messages = [genai.types.Content(role="user", parts=[genai.types.Part(text=prompt)]),]
+
+        response = client.models.generate_content(
+                model="gemini-2.0-flash-001",
+                contents=messages)
+        print(response.text)
+        print("Response Usage Metadata:")
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    except IndexError:
+        print("Usage: python main.py <prompt>")
+        sys.exit(1)
+
+    
 
 if __name__ == "__main__":
-    main()
+
+    main() 
